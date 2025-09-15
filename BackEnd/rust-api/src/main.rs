7a45156bdad4c221cf6181dcd2115f0800feb300
josephkb87 <kiyinijoseph@gmail.com>
2025-09-15 15:01:43 +0300

    Update cargo

--- BackEnd/rust-api/src/main.rs
+++ BackEnd/rust-api/src/main.rs
@@ -10,7 +10,7 @@ async fn hello() -> &'static str {
 async fn main() {
     let app = Router::new().route("/hello", get(hello));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8001));
-     let server = Server::bind(addr).serve(Router);
+    let server = Server::bind(addr).serve(route);
     println!("Rust backend running at http://{}", addr);
     Server::bind(addr)
         .serve(app.into_make_service())
