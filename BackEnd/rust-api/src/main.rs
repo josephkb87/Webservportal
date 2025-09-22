0ba4a69ec89db27a36193d137c523861d768b9bc
josephkb87 <kiyinijoseph@gmail.com>
2025-09-22 12:04:53 +0300

    Chore: Update Cargo

--- BackEnd/rust-api/src/main.rs
+++ BackEnd/rust-api/src/main.rs
@@ -1,5 +1,6 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use axum_server::Server;
 
 async fn hello() -> &'static str {
     "Hello from Rust Backend 🚀"
@@ -10,7 +11,7 @@ async fn main() {
     let app = Router::new().route("/hello", get(hello));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8001));
     println!("Rust backend running at http://{}", addr);
-    axum::Server::bind(&addr)
+    Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
