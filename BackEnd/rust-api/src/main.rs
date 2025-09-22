e55b057fe25b1dcb367b83f95e0ffacb8b897629
josephkb87 <kiyinijoseph@gmail.com>
2025-09-22 12:06:10 +0300

    Chore: Update Cargo

--- BackEnd/rust-api/src/main.rs
+++ BackEnd/rust-api/src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
     let app = Router::new().route("/hello", get(hello));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8001));
     println!("Rust backend running at http://{}", addr);
-    Server::bind(&addr)
+    Server::bind(addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
